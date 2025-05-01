c2ad4b93cb33d9a7dbdd4397f305960d5a8be9ac
lajoc_devs <fisayobolaji7@gmail.com>
2025-05-01 05:30:48 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/Signup-Form",
-})
+	plugins: [react()],
+	base: "/signup-form",
+});
